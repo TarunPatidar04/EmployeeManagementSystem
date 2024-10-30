c54b4fdb9a0df2bdca32e0b9f65e1fba190e9ef5
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-10-30 15:12:09 +0530

    create a Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import Login from "./components/Auth/Login";
 
 const App = () => {
   return (
-    <div className="bg-red-400">App</div>
-  )
-}
+    <>
+      <div>
+        <Login />
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+    setEmail("");
+    setPassword("");
+  };
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="border-2 rounded-xl border-emerald-600 p-20">
+        <form
+          action=""
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+          className="flex flex-col items-center justify-center"
+        >
+          <input
+            type="email"
+            required
+            placeholder="Enter your Email"
+            className="outline-none bg-transparent border-2 border-emerald-600 py-3 px-5 text-xl rounded-full placeholder:text-gray-400"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            required
+            placeholder="Enter your Password"
+            className="outline-none bg-transparent border-2 border-emerald-600 py-3 px-5 text-xl mt-3 rounded-full placeholder:text-gray-400"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button className=" mt-5 outline-none border-none  bg-emerald-600 py-2 px-8 w-full text-xl rounded-full placeholder:text-white">
+            Log in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
