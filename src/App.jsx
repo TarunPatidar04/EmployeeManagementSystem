1fdfce7f5615d91dc599f386e97a07081df0994c
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-11-03 10:41:48 +0530

    create a Header and Employee DashBoard

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Login from "./components/Auth/Login";
+import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 
 const App = () => {
   return (
     <>
       <div>
-        <Login />
+        {/* <Login /> */}
+        <EmployeeDashboard/>
       </div>
     </>
   );
